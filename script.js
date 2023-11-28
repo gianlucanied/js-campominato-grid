41aedd3082b92df499ffe3c11d2bd95744cf9e8c
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-28 17:08:57 +0100

    Add section JS

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-// Creiamo un container
-const container = document.querySelector (".container");
+/*L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+*/
 
-// Diciamo che il container contiene numeri da 1 a 100 e li stampiamo
+const startButton = document.querySelector (".button-start");
 
-for (let i=1; i<=100; i++) {
+const main = document.querySelector ("main");
 
-    console.log(i);
-    const square = document.createElement("div");
-    square.classList.add ("square");
-    square.append(i);
-    container.append (square);
+function showContainer() {
+
+    const container = document.createElement("div");
+    container.classList.add ("container");
+    main.append (container);
+
+    for (let i=1; i<=100; i++) {
+
+        const square = document.createElement("div");
+        square.classList.add ("square");
+        square.append(i);
+        container.append (square);
+
+        
+    square.addEventListener("click",() => {
+        console.log("Ho selezionato lo square numero ",i);
+        square.classList.add ("clicked");
 }
+)
+    }
+    startButton.disabled = true;
+}
+
+startButton.addEventListener("click", showContainer);
